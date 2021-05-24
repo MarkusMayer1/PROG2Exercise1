980ee1d6daf68d6c6cf074b7bb86d52dba83e79d
Markus Mayer <markus.mayer1@stud.fh-campuswien.ac.at>
2021-05-25 00:21:13 +0200

    Implemented Method checkSameNumbersInARow

--- src/main/java/at/ac/fhcampuswien/PasswordValidator.java
+++ src/main/java/at/ac/fhcampuswien/PasswordValidator.java
@@ -35,6 +35,6 @@ public class PasswordValidator {
     }
 
     public boolean checkSameNumbersInARow() {
-        return false;
+        return !Pattern.compile("(0000|1111|2222|3333|4444|5555|6666|7777|8888|9999)").matcher(password).find();
     }
 }
