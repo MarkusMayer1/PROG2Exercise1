20e8dcecf5167b035bd6845e9f09b38337659a7e
Markus Mayer <markus.mayer1@stud.fh-campuswien.ac.at>
2021-06-02 14:48:35 +0200

    changed from passing password by constructor to passing password by method parameter

--- src/main/java/at/ac/fhcampuswien/PasswordValidator.java
+++ src/main/java/at/ac/fhcampuswien/PasswordValidator.java
@@ -3,38 +3,32 @@ package at.ac.fhcampuswien;
 import java.util.regex.Pattern;
 
 public class PasswordValidator {
-    private String password;
-
-    public PasswordValidator(String password) {
-        this.password = password;
-    }
-
-    public boolean checkLength() {
+    public boolean checkLength(String password) {
         return password.length() >= 8 && password.length() <= 25;
     }
 
-    public boolean checkLowerCase() {
+    public boolean checkLowerCase(String password) {
         return Pattern.compile("[a-z]").matcher(password).find();
     }
 
-    public boolean checkUpperCase() {
+    public boolean checkUpperCase(String password) {
         return Pattern.compile("[A-Z]").matcher(password).find();
     }
 
-    public boolean checkNumber() {
+    public boolean checkNumber(String password) {
         return Pattern.compile("[0-9]").matcher(password).find();
     }
 
-    public boolean checkSpecialCharacter() {
+    public boolean checkSpecialCharacter(String password) {
         if (!password.replaceAll("[a-zA-Z0-9()#$?!%/@]", "").equals("")) return false;
         return Pattern.compile("[()#$?!%/@]").matcher(password).find();
     }
 
-    public boolean checkConsecutiveNumbers() {
+    public boolean checkConsecutiveNumbers(String password) {
         return !Pattern.compile("(012|123|234|345|456|567|789)").matcher(password).find();
     }
 
-    public boolean checkSameNumbersInARow() {
+    public boolean checkSameNumbersInARow(String password) {
         return !Pattern.compile("(0000|1111|2222|3333|4444|5555|6666|7777|8888|9999)").matcher(password).find();
     }
 }
--- src/test/java/at/ac/fhcampuswien/PasswordValidatorTest.java
+++ src/test/java/at/ac/fhcampuswien/PasswordValidatorTest.java
@@ -1,5 +1,6 @@
 package at.ac.fhcampuswien;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,213 +9,188 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordValidatorTest {
     private PasswordValidator passwordValidator;
 
+    @BeforeEach
+    void setup() {
+        passwordValidator = new PasswordValidator();
+    }
+
     @Test
     @DisplayName("Password to short")
     void checkLength_toShort() {
-        passwordValidator = new PasswordValidator("Max");
-        assertFalse(passwordValidator.checkLength());
+        assertFalse(passwordValidator.checkLength("Max"));
     }
 
     @Test
     @DisplayName("Password has valid length")
     void checkLength_valid() {
-        passwordValidator = new PasswordValidator("MaxMustermann");
-        assertTrue(passwordValidator.checkLength());
+        assertTrue(passwordValidator.checkLength("MaxMustermann"));
     }
 
     @Test
     @DisplayName("Password to long")
     void checkLength_toLong() {
-        passwordValidator = new PasswordValidator("MaxMustermannErikaMustermann");
-        assertFalse(passwordValidator.checkLength());
+        assertFalse(passwordValidator.checkLength("MaxMustermannErikaMustermann"));
     }
 
     @Test
     @DisplayName("Password has no upper case letter")
     void checkUpperCase_Scenario1() {
-        passwordValidator = new PasswordValidator("maxmustermann");
-        assertFalse(passwordValidator.checkUpperCase());
+        assertFalse(passwordValidator.checkUpperCase("maxmustermann"));
     }
 
     @Test
     @DisplayName("Password has upper case letter")
     void checkUpperCase_Scenario2() {
-        passwordValidator = new PasswordValidator("MaxMustermann");
-        assertTrue(passwordValidator.checkUpperCase());
+        assertTrue(passwordValidator.checkUpperCase("MaxMustermann"));
     }
 
     @Test
     @DisplayName("Password has no lower case letter")
     void checkLowerCase_Scenario1() {
-        passwordValidator = new PasswordValidator("MAXMUSTERMANN");
-        assertFalse(passwordValidator.checkLowerCase());
+        assertFalse(passwordValidator.checkLowerCase("MAXMUSTERMANN"));
     }
 
     @Test
     @DisplayName("Password has lower case letter")
     void checkLowerCase_Scenario2() {
-        passwordValidator = new PasswordValidator("MaxMustermann");
-        assertTrue(passwordValidator.checkLowerCase());
+        assertTrue(passwordValidator.checkLowerCase("MaxMustermann"));
     }
 
     @Test
     @DisplayName("Password has no number")
     void checkNumber_Scenario1() {
-        passwordValidator = new PasswordValidator("MaxMustermann");
-        assertFalse(passwordValidator.checkNumber());
+        assertFalse(passwordValidator.checkNumber("MaxMustermann"));
     }
 
     @Test
     @DisplayName("Password has number")
     void checkNumber_Scenario2() {
-        passwordValidator = new PasswordValidator("MaxMustermann5");
-        assertTrue(passwordValidator.checkNumber());
+        assertTrue(passwordValidator.checkNumber("MaxMustermann5"));
     }
 
     @Test
     @DisplayName("Password has no special character")
     void checkSpecialCharacter_Scenario1() {
-        passwordValidator = new PasswordValidator("MaxMustermann");
-        assertFalse(passwordValidator.checkSpecialCharacter());
+        assertFalse(passwordValidator.checkSpecialCharacter("MaxMustermann"));
     }
 
     @Test
     @DisplayName("Password has allowed special character (")
     void checkSpecialCharacter_Scenario2() {
-        passwordValidator = new PasswordValidator("MaxMustermann(");
-        assertTrue(passwordValidator.checkSpecialCharacter());
+        assertTrue(passwordValidator.checkSpecialCharacter("MaxMustermann("));
     }
 
     @Test
     @DisplayName("Password has allowed special character )")
     void checkSpecialCharacter_Scenario3() {
-        passwordValidator = new PasswordValidator("MaxMusterman)n");
-        assertTrue(passwordValidator.checkSpecialCharacter());
+        assertTrue(passwordValidator.checkSpecialCharacter("MaxMusterman)n"));
     }
 
     @Test
     @DisplayName("Password has allowed special character #")
     void checkSpecialCharacter_Scenario4() {
-        passwordValidator = new PasswordValidator("MaxMusterma#nn");
-        assertTrue(passwordValidator.checkSpecialCharacter());
+        assertTrue(passwordValidator.checkSpecialCharacter("MaxMusterma#nn"));
     }
 
     @Test
     @DisplayName("Password has allowed special character $")
     void checkSpecialCharacter_Scenario5() {
-        passwordValidator = new PasswordValidator("MaxMusterm$ann");
-        assertTrue(passwordValidator.checkSpecialCharacter());
+        assertTrue(passwordValidator.checkSpecialCharacter("MaxMusterm$ann"));
     }
 
     @Test
     @DisplayName("Password has allowed special character ?")
     void checkSpecialCharacter_Scenario6() {
-        passwordValidator = new PasswordValidator("MaxMuster?mann");
-        assertTrue(passwordValidator.checkSpecialCharacter());
+        assertTrue(passwordValidator.checkSpecialCharacter("MaxMuster?mann"));
     }
 
     @Test
     @DisplayName("Password has allowed special character %")
     void checkSpecialCharacter_Scenario7() {
-        passwordValidator = new PasswordValidator("MaxMuster%mann");
-        assertTrue(passwordValidator.checkSpecialCharacter());
+        assertTrue(passwordValidator.checkSpecialCharacter("MaxMuster%mann"));
     }
 
     @Test
     @DisplayName("Password has allowed special character /")
     void checkSpecialCharacter_Scenario8() {
-        passwordValidator = new PasswordValidator("MaxMuste/rmann");
-        assertTrue(passwordValidator.checkSpecialCharacter());
+        assertTrue(passwordValidator.checkSpecialCharacter("MaxMuste/rmann"));
     }
 
     @Test
     @DisplayName("Password has allowed special character @")
     void checkSpecialCharacter_Scenario9() {
-        passwordValidator = new PasswordValidator("@MaxMustermann");
-        assertTrue(passwordValidator.checkSpecialCharacter());
+        assertTrue(passwordValidator.checkSpecialCharacter("@MaxMustermann"));
     }
 
     @Test
     @DisplayName("Password has not allowed special character |")
     void checkSpecialCharacter_Scenario10() {
-        passwordValidator = new PasswordValidator("Max|Mustermann");
-        assertFalse(passwordValidator.checkSpecialCharacter());
+        assertFalse(passwordValidator.checkSpecialCharacter("Max|Mustermann"));
     }
 
     @Test
     @DisplayName("Password has not allowed special character *")
     void checkSpecialCharacter_Scenario11() {
-        passwordValidator = new PasswordValidator("MaxMustermann*");
-        assertFalse(passwordValidator.checkSpecialCharacter());
+        assertFalse(passwordValidator.checkSpecialCharacter("MaxMustermann*"));
     }
 
     @Test
     @DisplayName("Password has not allowed special character +")
     void checkSpecialCharacter_Scenario12() {
-        passwordValidator = new PasswordValidator("Ma+xMustermann");
-        assertFalse(passwordValidator.checkSpecialCharacter());
+        assertFalse(passwordValidator.checkSpecialCharacter("Ma+xMustermann"));
     }
 
     @Test
     @DisplayName("Password has not allowed special character -")
     void checkSpecialCharacter_Scenario13() {
-        passwordValidator = new PasswordValidator("MaxMuster-mann");
-        assertFalse(passwordValidator.checkSpecialCharacter());
+        assertFalse(passwordValidator.checkSpecialCharacter("MaxMuster-mann"));
     }
 
     @Test
     @DisplayName("Password has not allowed special character .")
     void checkSpecialCharacter_Scenario14() {
-        passwordValidator = new PasswordValidator("MaxMu.stermann");
-        assertFalse(passwordValidator.checkSpecialCharacter());
+        assertFalse(passwordValidator.checkSpecialCharacter("MaxMu.stermann"));
     }
 
     @Test
     @DisplayName("Password has not allowed special character ,")
     void checkSpecialCharacter_Scenario15() {
-        passwordValidator = new PasswordValidator("MaxMust,ermann");
-        assertFalse(passwordValidator.checkSpecialCharacter());
+        assertFalse(passwordValidator.checkSpecialCharacter("MaxMust,ermann"));
     }
 
     @Test
     @DisplayName("Password has allowed and not allowed special characters :)")
     void checkSpecialCharacter_Scenario16() {
-        passwordValidator = new PasswordValidator("MaxMustermann:)");
-        assertFalse(passwordValidator.checkSpecialCharacter());
+        assertFalse(passwordValidator.checkSpecialCharacter("MaxMustermann:)"));
     }
 
     @Test
     @DisplayName("Password has allowed and not allowed special characters (@-->)")
     void checkSpecialCharacter_Scenario17() {
-        passwordValidator = new PasswordValidator("(@Max-Must-ermann>)");
-        assertFalse(passwordValidator.checkSpecialCharacter());
+        assertFalse(passwordValidator.checkSpecialCharacter("(@Max-Must-ermann>)"));
     }
 
     @Test
     @DisplayName("Password has not more than two consecutive numbers")
     void checkConsecutiveNumbers_Scenario1() {
-        passwordValidator = new PasswordValidator("Max56Mustermann");
-        assertTrue(passwordValidator.checkConsecutiveNumbers());
+        assertTrue(passwordValidator.checkConsecutiveNumbers("Max56Mustermann"));
     }
 
     @Test
     @DisplayName("Password has more than two consecutive numbers")
     void checkConsecutiveNumbers_Scenario2() {
-        passwordValidator = new PasswordValidator("Max567Mustermann");
-        assertFalse(passwordValidator.checkConsecutiveNumbers());
+        assertFalse(passwordValidator.checkConsecutiveNumbers("Max567Mustermann"));
     }
 
     @Test
     @DisplayName("Password does not have the same number more than three times in a row")
     void checkSameNumbersInARow_Scenario1() {
-        passwordValidator = new PasswordValidator("Max555Mustermann");
-        assertTrue(passwordValidator.checkSameNumbersInARow());
+        assertTrue(passwordValidator.checkSameNumbersInARow("Max555Mustermann"));
     }
 
     @Test
     @DisplayName("Password has the same number more than three times in a row")
     void checkSameNumbersInARow_Scenario2() {
-        passwordValidator = new PasswordValidator("Max5555Mustermann");
-        assertFalse(passwordValidator.checkSameNumbersInARow());
+        assertFalse(passwordValidator.checkSameNumbersInARow("Max5555Mustermann"));
     }
 }
